eb77e9f6f7bbbdfd7ba6894ef921dad6d7944f9a
a.buzuverov <a.buzuverov@localhost.com>
2025-06-16 13:34:57 +0300

    Обновление

--- include/SqliteAdapter/sqlitetransaction.h
+++ include/SqliteAdapter/sqlitetransaction.h
@@ -5,7 +5,7 @@
 
 namespace database_adapter {
 
-class sqlite_transaction : public ITransaction
+class sqlite_transaction final : public ITransaction
 {
 public:
     explicit sqlite_transaction(sqlite3* database);
